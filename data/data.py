a9ece5b42471bd8ccca2d04a1e23d35d2ea21c41
parvvaresh <parvvaresh@gmail.com>
2024-03-30 16:32:21 +0330

    add class for this code

--- data/data.py
+++ data/data.py
@@ -1,1 +1,13 @@
-#test
+import pandas as pd
+from get_report_of_data.extraction_information import extraction_information
+from generate_date.generate_date import GenerateDate
+
+class data:
+    def __init__(self, data : pd.DataFrame) -> None:
+        self.data = data
+        self.report_data = extraction_information() 
+        self.generate_date = GenerateDate
+        # add more feature for this code
+        
+    def fit(self):
+        pass
